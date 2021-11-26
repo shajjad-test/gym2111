bd6578637bee826a93d06f64afb9fffa029f4098
shajjad-test <shajjad.test123@gmail.com>
2021-11-26 20:30:57 +0600

    member structure done

--- js/custom.js
+++ js/custom.js
@@ -9,4 +9,12 @@ $(document).ready(function(){
 
     //about video pop up
     $('.venobox').venobox(); 
+
+    //banner slider;
+    $('.testimonial-slider').slick({
+        arrows: false,
+        dots: true,
+        slidesToShow: 2,
+    });
+
 });
